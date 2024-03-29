63492b0c01fba94731da1fdc68f0275982b87ba3
Core447 <jmulmann@protonmail.com>
2024-03-29 07:48:07 +0100

    Fix: Using sessions bus instead of sytem bus

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ class BatteryPercentage(ActionBase):
         self.on_tick()
 
     def get_devices(self, fix_charging_duplicates: bool = True) -> dict:
-        bus = dbus.SessionBus()
+        bus = dbus.SystemBus()
         upower = bus.get_object('org.freedesktop.UPower', '/org/freedesktop/UPower')
         iface = dbus.Interface(upower, 'org.freedesktop.UPower')
 
